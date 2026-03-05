5b912ab55afcaad1b4c40ab61dc5ae01fb3c4f6d
FarAwayJams <159678961+FarAway6834@users.noreply.github.com>
2026-03-06 08:25:24 +0900

    setup.py 업데이트

--- setup.py
+++ setup.py
@@ -2,9 +2,10 @@ from setuptools import setup
 
 setup(
     name="privathon",
-    version="0.0.1",
+    version="0.0.2",
     description = "private scoped python",
     py_modules=["privation"],
+    install_requires=["martialLaw"],
     author='du7ec',
     author_email='dutec6834@gmail.com',
     keywords=["privathion", "private", "scoped", "python"]
