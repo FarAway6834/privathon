8184672dedb4f67d14fbb8400b115d4f3816433b
FarAway6834 <faway6834@gmail.com>
2026-08-17 17:20:14 +0900

    now I shold make note file for note dev version

--- privathon.py
+++ privathon.py
@@ -6,6 +6,8 @@ from martialaw.martialaw import partial as __partial__
 from functools import wraps as __smart_deco_wraps__
 import builtins as __builtin__
 
+__import__("warnings").warn("recommended not to use. it's version 0.0.2... that's indev. indev(fix bug)-infdev(make function scope as privatly) dev age is written in repo. come to see if you want! :)")
+
 """
 # privathon.py
 
@@ -260,13 +262,13 @@ class AmamiyaGoro:
 """
 
 @__clsr__
-__builtin__.__set_builtin_scope__ = lambda name, value : setattr(__builtin__, name, value)
+__builtin__.__set_builtin_scope__ = lambda name, value : setattr(__builtin__, name, value) #plz make this cythonic. str * obj -> Nonetype
 
 @__set_builtin_scope__("__builtin_scope__")
-__on_builtin_scope__ = lambda named_obj : __set_builtin_scope__(named_obj.__name__, named_obj)
+__on_builtin_scope__ = lambda named_obj : __set_builtin_scope__(named_obj.__name__, named_obj) #this is also obj -> Nonetype
 
 @__set_builtin_scope__("call_constant_functor")
-call_constant_functor = lambda f : f()
+call_constant_functor = lambda f : f() #this also object -> object
 
 @__builtin_scope__
 class ConstantError(Exception):
@@ -281,7 +283,7 @@ class ConstantError(Exception):
     pass
 
 @__on_builtin_scope__("constant_err")
-constant_err = ConstantError("const is immutable")
+constant_err = ConstantError("const is immutable") # are you crazy? it's too easy to crack
 
 def raise_constant_err():
     """
@@ -291,10 +293,10 @@ def raise_constant_err():
 
      - fin -
     """
-    raise constant_err
+    raise constant_err # fuck... it's too easy to crack... please wrap as c void function
 
 @__set_builtin_scope__("salt")
-salt = lambda : hash(str(__unix_time__() + __random_of_unit_interval__()))
+salt = lambda : hash(str(__unix_time__() + __random_of_unit_interval__())) #it's too easy to crack... please wrap as c void function
 
 class private(type):
     """
@@ -351,10 +353,10 @@ class private(type):
     
     @staticmethod
     @__clsr__
-    seal = lambda getter, setter : property(fget = getter, fset = setter)
+    seal = lambda getter, setter : property(fget = getter, fset = setter) # it's too easy to hack... damb. I've no idea 2 solve this problem
 
     @staticmethod
-    const = lambda constant_function : private.seal(constant_function)(raise_constant_err)
+    const = lambda constant_function : private.seal(constant_function)(raise_constant_err) # it's too easy to crack or neutralize... I give up. go last line
 
     @staticmethod
     def sealed_value(var):
@@ -380,6 +382,7 @@ static = lambda __static__, func : __smart_deco_wraps__(__partial__(func, __stat
     static_decocls = lambda cls : static(call_constant_functor(cls))
     
     def __new__(metacls, name, *argv):
+
         """
         # privathon private class constructor
         
@@ -388,25 +391,81 @@ static = lambda __static__, func : __smart_deco_wraps__(__partial__(func, __stat
         
          - fin -
         """
+
         L = len(argv)
+
         assert L * L == L + L, f"private get 1 or 3 arguments but {L} given" # L(L - 2) = 0 ↔ L = 0 ∨ L = 2
         
         if L: # L == 2. check "as constructor
             __dict__ = argv[1]
+
             @const
-            def private(self, private_wraps = {}):
+            def private(self, private_wraps = {}): # how2works? : hide in getter's arugment
                 selfid = id(self)
-                if selfid in private_wraps: return private_wraps[selfid]
+
+                if selfid in private_wraps: return private_wraps[selfid] # same ob becuase same address
                 else:
-                    this = self
-                    __private__ = __dict__["private"]()
+                    this = self # this method's self
+
+                    __private__ = __dict__["private"]() # function private's value
+
+                    # object's attribute "private" is actually delete it self, and return object which "this"
                     @const
                     def private(self):
-                        del self
+                        del self # pop the private-wrapper objects on "computer memory"
                         return this
+
+                    # when delete the object, private_wraps's object is also unregisterd
                     def __del__(self):
-                        __dict__["__del__"](self)
+                        __dict__["__del__"](self) # for method "__del__"
                         del private_wraps[selfid]
-                    return type("PrivatrWrapper", (), {i : (private if i == "private" else (__del__ if i == "__del__" else (__smart_deco_wraps__(j)(__partial__(j, this = self, __private__ = __private__)) if callable(j) else j)) for i, j in __dict__.items() if i != "__init__"}
-           return type(metacls, name, argv[0], {i : private if i == "private" else (__smart_deco_wraps__(j)(__parital__(j, this = metacls, __private__ = metacls)) if callable(j) else j for i, j in __dict__.items()}
+
+                    # it returns PrivateWrapper Object. and this src is actually class gen perttern for complete privaty
+                    return type(
+                            "PrivateWrapper", 
+                            (), 
+                            {
+                                i
+                                :
+                                (
+                                    private if i == "private" else ( # attribute "private"
+                                        __del__ if i == "__del__" else ( # method "__del__"
+                                            __smart_deco_wraps__(
+                                                j # this function get "this" and "__private__"
+                                            )(
+                                                __partial__(
+                                                    j,
+                                                    this = self, # refer which "ORIGIN"... actually this src pack in getter as attribuate name "private" to which obj "ORIGIN"
+                                                    __private__ = __private__ # class src's private()'s value. generally dictionarry because it's purpose is for dict or list. it's object's private fields
+                                                )
+                                            )
+                                        ) if callable(j) else j)
+                                )
+                                for i, j in __dict__.items()
+                                if i != "__init__" # no __private__ fields in __init__. because __init__ is before __private__ field
+                            }
+                        )
+           return type(
+                   metacls,
+                   name,
+                   argv[0],
+                   {
+                       i
+                       :
+                       private if i == "private" else ( # cls.private is generating private-wrapper and return that.
+                           __smart_deco_wraps__(
+                               j
+                            )(
+                                __parital__(
+                                    j,
+                                    this = metacls, # this refer metaclass... it's dummy datas. because non private-wrapper object has no non-private version. this is non-private version. this is "ORIGIN"
+                                    __private__ = metacls #privater metaclass. because non private-wrapper object has no __private__ field
+                                )
+                            ) if callable(j) else j
+                        )
+                       for i, j in __dict__.items()
+                    }
+                ) # if this is private, then self is "ORIGIN", if is not, this is "ORIGIN"
         else: return name.private # if L == 1 then just return private. check "as function"
+
+# function scope safe lock lib is inevitable
